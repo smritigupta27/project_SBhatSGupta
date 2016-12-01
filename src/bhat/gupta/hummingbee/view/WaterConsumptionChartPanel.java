db8fb9368f955213c8956c898489650269e04c64
Smriti Gupta <smritigupta27@gmail.com>
2016-11-30 23:02:37 -0800

    Added legend to graph and axis labels

--- src/bhat/gupta/hummingbee/view/WaterConsumptionChartPanel.java
+++ src/bhat/gupta/hummingbee/view/WaterConsumptionChartPanel.java
@@ -1,6 +1,7 @@
 package bhat.gupta.hummingbee.view;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.util.*;
 import java.util.List;
 import java.util.Map.Entry;
@@ -9,17 +10,16 @@ import javax.swing.JPanel;
 
 public class WaterConsumptionChartPanel extends JPanel {
 	private int panelHeight;
-	private int panelWidth;
-	private static final int BORDER_GAP = 30;
-	private static final Color GRAPH_POINT_COLOR = new Color(150, 50, 50, 180);
+	private int panelWidth,barPanelWidth,legendPanelWidth;
+	private static final int BORDER_GAP = 35;
 	private static final Stroke GRAPH_STROKE = new BasicStroke(3f);
 	private static final int GRAPH_POINT_WIDTH = 8;
-	private static final int Y_HATCH_CNT = 10;
 	private static final int MAX_DAYS = 7;
 	private Map<String, Color> graphColorByZone;
 	private Map<String, Map<String, Double>> dayVolumeMapByZones;
 	Map<String, List<Point>> graphPointsByZone;
 	private int max_Volume;
+	private Graphics2D g2;
 
 	public WaterConsumptionChartPanel(Map<String, Map<String, Double>> dayVolumeMapByZones) {
 		createGraphColorByZoneMap();
@@ -34,7 +34,7 @@ public class WaterConsumptionChartPanel extends JPanel {
 		this.graphColorByZone.put("EAST", Color.GREEN);
 		this.graphColorByZone.put("WEST", Color.RED);
 		this.graphColorByZone.put("NORTH", Color.BLUE);
-		this.graphColorByZone.put("SOUTH", Color.YELLOW);
+		this.graphColorByZone.put("SOUTH", Color.GRAY);
 	}
 
 	public void findMaxVolume() {
@@ -51,15 +51,25 @@ public class WaterConsumptionChartPanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
+//		Graphics2D g2 = (Graphics2D) g;
+		g2 = (Graphics2D) g;
 		Dimension df = this.getSize();
 		panelHeight = (int) df.getHeight();
 		panelWidth = (int) df.getWidth();
+		barPanelWidth=panelWidth / 4 * 3;
+		legendPanelWidth = panelWidth / 4;
 
+		createLineGraph();
+		createLegend();
+
+	}
+	public void createLineGraph()
+	{
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 
-		double xScale = ((double) getWidth() - 2 * BORDER_GAP) / MAX_DAYS;
+//		double xScale = ((double) getWidth() - 2 * BORDER_GAP) / MAX_DAYS;
+		double xScale = ((double) barPanelWidth - BORDER_GAP) / MAX_DAYS;
 		double yScale = ((double) getHeight() - 2 * BORDER_GAP)
 				/ (this.max_Volume - 1);
 
@@ -81,10 +91,30 @@ public class WaterConsumptionChartPanel extends JPanel {
 		}
 
 		// create x and y axes
-		g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, BORDER_GAP,
-				BORDER_GAP);
-		g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, getWidth()
-				- BORDER_GAP, getHeight() - BORDER_GAP);
+		int axisStartPointX = BORDER_GAP;
+		int axisStartPointY = getHeight() - BORDER_GAP;
+		int xAxisEndPointX = getWidth()-legendPanelWidth;
+		int xAxisEndPointY = getHeight() - BORDER_GAP;
+		int yAxisEndPointX = BORDER_GAP;
+		int yAxisEndPointY = BORDER_GAP;
+		g2.drawLine(axisStartPointX, axisStartPointY, xAxisEndPointX, xAxisEndPointY);
+		g2.drawLine(axisStartPointX, axisStartPointY, yAxisEndPointX, yAxisEndPointY);
+		
+		// Write Topic Name
+				Font font = new Font("Arial", Font.ITALIC, 12);
+				g2.setFont(font);
+				g2.setPaint(Color.BLACK);
+				FontMetrics metrics = g2.getFontMetrics(font);
+				String xAxisName = "DAYS OF THE WEEK";
+				int textWidthHf = metrics.stringWidth(xAxisName) / 2;
+				int textAscent = metrics.getMaxAscent();
+				int startPointX = (barPanelWidth/2)-textWidthHf;
+				g2.drawString(xAxisName, startPointX, getHeight() - textAscent);
+				
+				String yAxisName="litres ";
+				textWidthHf = metrics.stringWidth(yAxisName) / 2;
+				startPointX = BORDER_GAP-metrics.stringWidth(yAxisName);
+				g2.drawString(yAxisName, startPointX, getHeight()/2);
 
 		Stroke oldStroke = g2.getStroke();
 		for (Entry<String, List<Point>> entry : graphPointsByZone.entrySet()) {
@@ -110,6 +140,32 @@ public class WaterConsumptionChartPanel extends JPanel {
 				g2.fillOval(x, y, ovalW, ovalH);
 			}
 
+		}
+	}
+	public void createLegend() {
+
+		// create Font for Account name
+		Font font = new Font("SansSerif", Font.BOLD, 12);
+		g2.setFont(font);
+
+		// get FontMetrics for calculating offsets and
+		// positioning descriptions
+		FontMetrics metrics = getFontMetrics(font);
+		int ascent = metrics.getMaxAscent();
+		int startX = barPanelWidth;
+		int startY = BORDER_GAP;
+		int spaceBtwnLegends = 30;
+
+		// draw description for each data point
+		for(Entry<String,Color> e:this.graphColorByZone.entrySet())
+		{
+			g2.setColor(e.getValue());
+			g2.fillRect(startX, startY, ascent, ascent);
+			// Write Legend Description
+			int startX2 = startX + ascent * 2;
+			g2.setColor(Color.black);
+			g2.drawString(e.getKey(), startX2, startY+ascent);
+			startY = startY + spaceBtwnLegends;
 		}
 
 	}
